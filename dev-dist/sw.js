b379847c78badaad67035747a45856cb469bf488
Catalyst-101 <skyisblack95@gmail.com>
2026-06-01 17:13:20 +0500

    working: 2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-f7a01684'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.notq8tlubio"
+    "revision": "0.7c0u9imhgak"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
